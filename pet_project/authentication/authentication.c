b1c437053d8c59964085e83aeb4db0f9973cb1f9
kelvinthuo999 <thuokelvinngigi@gmail.com>
2023-06-14 15:54:21 +0000

    authentication progress

--- pet_project/authentication/authentication.c
+++ pet_project/authentication/authentication.c
@@ -10,21 +10,33 @@
 int main(void)
 {
 	int idx;
-	char answer[10];
+	char answer[50];
 	char *pta = answer;
 
 	printf("\t\t\t\t\tWelcome to the Authentication Page\n");
-	printf("\nLogin/Sign Up? \n");
-	fgets(answer, sizeof(answer), stdin);
+	printf("\nLogin/Sign Up? ");
+	scanf("%s", answer);
 
 	/* convert the input to uppercase */
 	for (idx = 0; idx < strlen(answer); idx++)
 	{
 		answer[idx] = toupper(answer[idx]);
 	}
-
-	printf("%s", answer);
 	
 	/* if statement to check input */
+	
+	if (strcmp(answer, "LOGIN") == 0)
+	{
+		printf("\t\t\t\tWelcome to the Login page\n");
+		printf("\nEnter username: ");
+		printf("\nEnter password: ");
+	}
+	if (strcmp(answer, "SIGN UP") == 0)
+	/*else if(strcmp(answer, "SIGN UP") == 0)*/
+	{
+		printf("\t\t\t\t Welcome to the sign up page\n");
+		printf("\nEnter username: ");
+		printf("\nEnter password: ");
+	}
 	return (0);
 }
