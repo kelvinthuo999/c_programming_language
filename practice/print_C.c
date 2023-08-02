b74ba3dfa21ba7c8428e52170eda08dc23d75c09
kelvinthuo999 <thuokelvinngigi@gmail.com>
2023-08-02 16:49:30 +0000

    print the letter C and F

---
+++ practice/print_C.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints the letter 'C'
+ * Return: 0(success)
+ */
+
+int main(void)
+{
+	printf("   ######\n ##      ##\n#\n#\n#\n#\n#\n ##      ##\n   ######\n");
+
+	return (0);
+}
---
+++ practice/print_F.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - print the letter F
+ * Return: 0(success)
+ */
+
+int main(void)
+{
+	printf("#####\n#\n#\n#####\n#\n#\n#\n");
+
+	return (0);
+}
